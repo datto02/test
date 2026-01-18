42b98afccb7d1fb35b50dc265ea8e64e9bb69f75
datto02 <vuthedat333@gmail.com>
2026-01-18 23:51:59 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -5,7 +5,7 @@ export const levelColors = {
     N2: 'bg-purple-100 text-purple-700 border-purple-200 hover:bg-purple-600 hover:text-white hover:border-purple-600',
     N1: 'bg-red-100 text-red-700 border-red-200 hover:bg-red-600 hover:text-white hover:border-red-600'
 };
-
+ 
 export const defaultConfig = { 
     text: '', fontSize: 35, traceCount: 9, verticalOffset: -3, 
     traceOpacity: 0.15, guideScale: 1.02, guideX: 0, guideY: 0.5, 
