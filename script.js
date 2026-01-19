19be745eb6b72ba477f81b53696c5821aa325f2f
datto02 <vuthedat333@gmail.com>
2026-01-19 23:16:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d").
 };
     const { useState, useEffect, useMemo, useRef } = React;
 
- // --- FETCH DATA FROM GITHUB ---
+ // --- FETCH DATA FROM GITHUB --- 
 const fetchDataFromGithub = async () => {
   try { 
     // Tải song song cả 2 file database
